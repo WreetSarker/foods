dc43f4b339c0806d41c937823b4d231b54c3a2e6
WreetSarker <wreet.sarker@gmail.com>
2021-02-19 10:45:29 +0600

    first commit after adding cards

---
+++ script.js
@@ -0,0 +1,12 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+let i = 1;
+setInterval(() => {
+    i++;
+    const icon = document.querySelector('.section-1-icons .change');
+    icon.classList.remove('change');
+    if (i > icons.length) {
+        icons[0].classList.add('change');
+        i = 1;
+    }
+    icon.nextElementSibling.classList.add('change');
+}, 4000);
